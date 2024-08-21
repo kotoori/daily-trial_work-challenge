d6ba291427e857d39ebbd03cbc6a390f12533894
kotoori <kotoori0815@gmail.com>
2024-08-21 12:29:02 +0900

    003：トップページ　FV　PC表示

---
+++ 003_day-maga/static/src/assets/js/top.js
@@ -0,0 +1,19 @@
+/*===============================
+swiper
+===============================*/
+const swiperFv = new Swiper('.js-top-fv-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+	centeredSlides: true,
+	// autoplay: {
+	// 	delay: 5000,
+	// },
+	slidesPerView: 'auto',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
