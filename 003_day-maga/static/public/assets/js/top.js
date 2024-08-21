b48bb06e0eb9ba69388793ca79137f091f2126fa
kotoori <kotoori0815@gmail.com>
2024-08-22 02:38:00 +0900

    003：トップページ　おすすめ記事セクション（PC表示の実装途中）

--- 003_day-maga/static/public/assets/js/top.js
+++ 003_day-maga/static/public/assets/js/top.js
@@ -6,14 +6,33 @@ const swiperFv = new Swiper('.js-top-fv-swiper', {
   direction: 'horizontal',
   loop: true,
 	centeredSlides: true,
-	// autoplay: {
-	// 	delay: 5000,
-	// },
+	autoplay: {
+		delay: 5000,
+	},
 	slidesPerView: 'auto',
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperPopular = new Swiper('.js-top-popular-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+	slidesPerView: 3.19,
+	spaceBetween: 32,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+	// Scrollbar
+	scrollbar: {
+    el: '.swiper-scrollbar',
   },
 });
