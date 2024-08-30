24662e5345403ad7dec7948fdb387ee0dac6b765
kotoori <kotoori0815@gmail.com>
2024-08-30 14:02:35 +0900

    003：修成

--- 003_day-maga/WP/app/public/wp-content/themes/daymaga/index.php
+++ 003_day-maga/WP/app/public/wp-content/themes/daymaga/index.php
@@ -1,5 +1,5 @@
 <?php 
-	global $dir; 
+	global $dir;
 	get_header();
 ?>
 
@@ -10,228 +10,82 @@
 				<div class="p-archive__content">
 					<div class="c-archive js-tab-parent">
 						<ul class="c-archive__tab-list">
-							<li class="c-archive__tab-item"><button class="c-tab --lightblue2 is-active js-tab" data-color="--lightblue2">すべて</button></li>
+							<li class="c-archive__tab-item"><button class="c-tab --lightblue2 is-active js-tab"data-category="all" data-color="--lightblue2">すべて</button></li>
 						</ul>
 						<div class="c-archive__card-area js-tab-target --lightblue2">
 							<div class="c-archive__content__change">
 								<div class="c-btn-change">
-									<button class="c-btn-change__item is-active js-point">新着順</button>
-									<button class="c-btn-change__item js-point">人気順</button>
+									<button class="c-btn-change__item is-active js-order" data-order="date">新着順</button>
+									<button class="c-btn-change__item js-order" data-order="views">人気順</button>
 								</div>
 							</div>
-							<div class="c-archive__cards">
-								<!-- card1 -->
+							<div class="c-archive__cards" id="js-archive__container">
+							<?php
+									//ここにajaxで取得した記事が表示される
+									//取得できなかった場合は、以下の記事を表示する
+									if(have_posts()): while(have_posts()): the_post();
+							?>
+								<?php
+									$category = get_the_category();
+									if(isset($category[0]->cat_name)){
+										$category_name =  $category[0]->cat_name;
+										$category_color = get_category_color($category[0]->slug);
+									}
+									if($category_color == ""){
+										$category_color = 'blue';
+									}
+								?>
 								<div class="c-archive__card">
 									<article class="c-card">
-										<a href="#" class="c-card__link">
+										<a href="<?php the_permalink(); ?>" class="c-card__link">
 											<div class="c-card__img">
-												<img src="./assets/img/card/img_card05.webp" alt="" width="1080" height="612">
+												<?php if(has_post_thumbnail()): ?>
+													<?php the_post_thumbnail(); ?>
+												<?php else: ?>
+														<img src="<?php echo $dir ;?>/assets/img/card/img_dummy.webp" alt="" width="1080" height="612" loading="lazy">
+												<?php endif; ?>
 											</div>
-											<time class="c-card__date" datetime="2024-07-15">2024.07.15</time>
-											<div class="c-card__title">フリーコンサルの現場から〜活動の実態と成功のポイント</div>
+											<time class="c-card__date" datetime="<?php the_time('c');?>"><?php the_time('Y.m.d'); ?></time>
+											<div class="c-card__title"><?php the_title(); ?></div>
 											<div class="c-card__category ">
-												<div class="c-category--coralblue">TIPS</div>
+												<div class="c-category--<?php echo $category_color; ?>"><?php echo $category_name; ?></div>
 											</div>
 											<ul class="c-card__tag ">
-												<li class="c-card__tag__item c-tag">#コンサルファーム</li>
-												<li class="c-card__tag__item c-tag">#共通</li>
-												<li class="c-card__tag__item c-tag">#その他</li>
-												<li class="c-card__tag__item c-tag">#Pickup</li>
+												<?php
+													//タグ取得
+													$tags = get_tags_by_order(get_the_ID());
+													if($tags): foreach($tags as $tag):
+												?>
+													<li class="c-card__tag__item c-tag">#<?php echo $tag->name;?></li>
+												<?php endforeach; endif;?>
 											</ul>
 										</a>
 									</article>
 								</div>
-								<!-- card2 -->
-								<div class="c-archive__card">
-									<article class="c-card">
-										<a href="#" class="c-card__link">
-											<div class="c-card__img">
-												<img src="./assets/img/card/img_card05.webp" alt="" width="1080" height="612">
-											</div>
-											<time class="c-card__date" datetime="2024-07-15">2024.07.15</time>
-											<div class="c-card__title">フリーコンサルの現場から〜活動の実態と成功のポイント</div>
-											<div class="c-card__category ">
-												<div class="c-category--coralblue">TIPS</div>
-											</div>
-											<ul class="c-card__tag ">
-												<li class="c-card__tag__item c-tag">#コンサルファーム</li>
-												<li class="c-card__tag__item c-tag">#共通</li>
-												<li class="c-card__tag__item c-tag">#その他</li>
-												<li class="c-card__tag__item c-tag">#Pickup</li>
-											</ul>
-										</a>
-									</article>
-								</div>
-								<!-- card3 -->
-								<div class="c-archive__card">
-									<article class="c-card">
-										<a href="#" class="c-card__link">
-											<div class="c-card__img">
-												<img src="./assets/img/card/img_card05.webp" alt="" width="1080" height="612">
-											</div>
-											<time class="c-card__date" datetime="2024-07-15">2024.07.15</time>
-											<div class="c-card__title">フリーコンサルの現場から〜活動の実態と成功のポイント</div>
-											<div class="c-card__category ">
-												<div class="c-category--coralblue">TIPS</div>
-											</div>
-											<ul class="c-card__tag ">
-												<li class="c-card__tag__item c-tag">#コンサルファーム</li>
-												<li class="c-card__tag__item c-tag">#共通</li>
-												<li class="c-card__tag__item c-tag">#その他</li>
-												<li class="c-card__tag__item c-tag">#Pickup</li>
-											</ul>
-										</a>
-									</article>
-								</div>
-								<!-- card4 -->
-								<div class="c-archive__card">
-									<article class="c-card">
-										<a href="#" class="c-card__link">
-											<div class="c-card__img">
-												<img src="./assets/img/card/img_card05.webp" alt="" width="1080" height="612">
-											</div>
-											<time class="c-card__date" datetime="2024-07-15">2024.07.15</time>
-											<div class="c-card__title">フリーコンサルの現場から〜活動の実態と成功のポイント</div>
-											<div class="c-card__category ">
-												<div class="c-category--coralblue">TIPS</div>
-											</div>
-											<ul class="c-card__tag">
-												<li class="c-card__tag__item c-tag">#コンサルファーム</li>
-												<li class="c-card__tag__item c-tag">#共通</li>
-												<li class="c-card__tag__item c-tag">#その他</li>
-												<li class="c-card__tag__item c-tag">#Pickup</li>
-											</ul>
-										</a>
-									</article>
-								</div>
-								<!-- card5 -->
-								<div class="c-archive__card">
-									<article class="c-card">
-										<a href="#" class="c-card__link">
-											<div class="c-card__img">
-												<img src="./assets/img/card/img_card05.webp" alt="" width="1080" height="612">
-											</div>
-											<time class="c-card__date" datetime="2024-07-15">2024.07.15</time>
-											<div class="c-card__title">フリーコンサルの現場から〜活動の実態と成功のポイント</div>
-											<div class="c-card__category ">
-												<div class="c-category--coralblue">TIPS</div>
-											</div>
-											<ul class="c-card__tag ">
-												<li class="c-card__tag__item c-tag">#コンサルファーム</li>
-												<li class="c-card__tag__item c-tag">#共通</li>
-												<li class="c-card__tag__item c-tag">#その他</li>
-												<li class="c-card__tag__item c-tag">#Pickup</li>
-											</ul>
-										</a>
-									</article>
-								</div>
-								<!-- card6 -->
-								<div class="c-archive__card">
-									<article class="c-card">
-										<a href="#" class="c-card__link">
-											<div class="c-card__img">
-												<img src="./assets/img/card/img_card05.webp" alt="" width="1080" height="612">
-											</div>
-											<time class="c-card__date" datetime="2024-07-15">2024.07.15</time>
-											<div class="c-card__title">フリーコンサルの現場から〜活動の実態と成功のポイント</div>
-											<div class="c-card__category ">
-												<div class="c-category--coralblue">TIPS</div>
-											</div>
-											<ul class="c-card__tag ">
-												<li class="c-card__tag__item c-tag">#コンサルファーム</li>
-												<li class="c-card__tag__item c-tag">#共通</li>
-												<li class="c-card__tag__item c-tag">#その他</li>
-												<li class="c-card__tag__item c-tag">#Pickup</li>
-											</ul>
-										</a>
-									</article>
-								</div>
-								<!-- card7 -->
-								<div class="c-archive__card">
-									<article class="c-card">
-										<a href="#" class="c-card__link">
-											<div class="c-card__img">
-												<img src="./assets/img/card/img_card05.webp" alt="" width="1080" height="612">
-											</div>
-											<time class="c-card__date" datetime="2024-07-15">2024.07.15</time>
-											<div class="c-card__title">フリーコンサルの現場から〜活動の実態と成功のポイント</div>
-											<div class="c-card__category ">
-												<div class="c-category--coralblue">TIPS</div>
-											</div>
-											<ul class="c-card__tag ">
-												<li class="c-card__tag__item c-tag">#コンサルファーム</li>
-												<li class="c-card__tag__item c-tag">#共通</li>
-												<li class="c-card__tag__item c-tag">#その他</li>
-												<li class="c-card__tag__item c-tag">#Pickup</li>
-											</ul>
-										</a>
-									</article>
-								</div>
-								<!-- card8 -->
-								<div class="c-archive__card">
-									<article class="c-card">
-										<a href="#" class="c-card__link">
-											<div class="c-card__img">
-												<img src="./assets/img/card/img_card05.webp" alt="" width="1080" height="612">
-											</div>
-											<time class="c-card__date" datetime="2024-07-15">2024.07.15</time>
-											<div class="c-card__title">フリーコンサルの現場から〜活動の実態と成功のポイント</div>
-											<div class="c-card__category ">
-												<div class="c-category--coralblue">TIPS</div>
-											</div>
-											<ul class="c-card__tag ">
-												<li class="c-card__tag__item c-tag">#コンサルファーム</li>
-												<li class="c-card__tag__item c-tag">#共通</li>
-												<li class="c-card__tag__item c-tag">#その他</li>
-												<li class="c-card__tag__item c-tag">#Pickup</li>
-											</ul>
-										</a>
-									</article>
-								</div>
+							<?php endwhile; endif;?><!-- hove_posts() -->
 							</div><!-- /.c-archive__cards -->
 						</div><!-- /.c-archive__card-area -->
 					</div><!-- /.c-archive -->
 				</div>
 				<div class="p-archive__pagenation">
-					<div class="c-pagination">
-						<a class="prev page-numbers" href="前ページへのリンク"></a>
-						<a class="page-numbers" href="1ページ目へのリンク">1</a>
-						<span class="page-numbers current">2</span>
-						<a class="page-numbers" href="3ページ目へのリンク">3</a>
-						<span class="page-numbers dots">&hellip;</span>
-						<a class="page-numbers" href="最後のページへのリンク">15</a>
-						<a class="next page-numbers" href="次ページへのリンク"></a>
+					<div class="c-pagination" id="js-pagination">
+						<?php
+							$args = array(
+								'end_size' => 0,
+								'mid_size' => 1,
+								'prev_next' => true,
+								'prev_text' => '',
+								'next_text' => '',
+							);
+							echo paginate_links($args); ?>
 					</div>
 				</div>
 			</div>
 		</section>
 		<div class="p-archive__search" id="a-search">
 			<div class="l-inner">
-				<div class="c-search">
-					<div class="c-search__title">キーワードで絞り込む</div>
-					<ul class="c-search__tag-list">
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#コンサルファーム</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#クライアント</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#共通</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#戦略</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#IT</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#業務改善</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#HR</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#M&A・事業再生</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#金融・保険</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#メーカー</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#商社</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#不動産・建設・設備</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#サービス</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#物流</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#流通</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#その他</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#広告・マスコミ</a></li>
-						<li class="c-search__tag-item"><a class="c-tag--link" href="">#Pickup</a></li>
-					</ul>
-				</div>
+				<?php get_template_part('template-parts/tag-list'); ?>
 			</div>
 		</div>
 	</main>
-	
 <?php get_footer(); ?>
